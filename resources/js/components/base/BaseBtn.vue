55f3d0daf5edbdc5a6151731dc237177a6e792a7
Ricardo Coelho <devel@rmorgado.ch>
2021-11-04 10:33:23 +0100

    frontend: add base card and btn

---
+++ resources/js/components/base/BaseBtn.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn depressed v-bind="$attrs" v-on="$listeners">
+      <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+    name: "BaseBtn"
+}
+</script>
---
+++ resources/js/components/base/BaseCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card outlined rounded flat v-bind="$attrs" v-on="$listeners">
+      <slot />
+  </v-card>
+</template>
+
+<script>
+export default {
+    name: "BaseCard"
+
+}
+</script>
